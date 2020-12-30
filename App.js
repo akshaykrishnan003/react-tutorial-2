b3d6ef99c3cfa1b0a5676a50647e56fdc41afb1c
akshaykrishnan003 <akshaykrishnan@ceknpy.ac.in>
2020-12-30 15:00:57 +0530

    complete

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: "one", id: "1" },
+    { name: "two", id: "2" },
+    { name: "three", id: "3" },
+    { name: "four", id: "4" },
+    { name: "five", id: "5" },
+    { name: "six", id: "6" },
+    { name: "seven", id: "7" },
+    { name: "eight", id: "8" },
+    { name: "nine", id: "9" },
+    { name: "ten", id: "10" },
+  ]);
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id != id);
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id) } >
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -14,8 +47,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    //alignItems: "center",
+    //justifyContent: "center",
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
   },
 });
